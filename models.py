efa3de300ca9859f3716531117078f5c0c6d846b
Daniel Ghadimi <dannyghadimi@gmail.com>
2020-06-17 17:34:12 +0100

    fix performances

--- models.py
+++ models.py
@@ -107,7 +107,7 @@ def associationupdate(movieid, actorid):
 Association table linking movies and actors in Performances 
 '''
 
-Performance = db.Table('performances', 
+Performance = db.Table('performances', db.Model.metadata,
   db.Column('Movie_id', db.Integer, db.ForeignKey('movies.id')), 
   db.Column('Actor_id', db.Integer, db.ForeignKey('actors.id')), 
   )
