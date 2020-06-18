6a9b2cdad25a78e9815728b6740162219b4fc2ec
Daniel Ghadimi <dannyghadimi@gmail.com>
2020-06-18 21:22:36 +0100

    Update setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+export AUTH0_DOMAIN="coffeedan.eu.auth0.com"
+export API_AUDIENCE="cap"
+export ALGORITHMS=["RS256"]
+export DATABASE_URL="postgres://qkemyejqagfnpt:ca6391feb2aa6ecd86b8cc4dffd4de1f554667ef0d2b3de77211631509cf90ca@ec2-34-202-88-122.compute-1.amazonaws.com:5432/dbulfli3d8a6t3"
